e99e5fa0e4294a511b32d1fede0121e4b3834f81
jorryns <122568624+jorryns@users.noreply.github.com>
2023-01-26 12:56:45 -0800

    fixed arrays

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,8 +5,16 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    
+    
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+    int[] input2 = {1, 2, 3,4 ,5, 6, 7, 8, 9 ,10};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{10,9,8,7,6,5,4,3,2,1}, input2);
+    int[] input3 = {};
+    ArrayExamples.reverseInPlace(input3);
+    assertArrayEquals(new int[]{}, input3);
 	}
 
 
@@ -14,5 +22,9 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input2 = {1,2,3,4,5,6,7,8,9,10};
+    assertArrayEquals(new int[]{10,9,8,7,6,5,4,3,2,1}, ArrayExamples.reversed(input2));
+    int[] input3 = {3};
+    assertArrayEquals(new int[]{3}, ArrayExamples.reversed(input3));
   }
 }
